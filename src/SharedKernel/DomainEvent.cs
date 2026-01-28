a1365d5bb26df3b3ac402b2c58b3cadf923aea8f
Burak Can <bc913dev@outlook.com>
2026-01-27 22:25:09 -0500

    Simplify domain event class

--- src/SharedKernel/DomainEvent.cs
+++ src/SharedKernel/DomainEvent.cs
@@ -5,20 +5,6 @@ namespace Bcan.Backend.SharedKernel
 {
     public abstract class DomainEvent : INotification
     {
-        protected DomainEvent()
-        {
-            DateOccurred = DateTimeOffset.UtcNow;
-        }
-
-        #region Fields & Properties
-        public DateTimeOffset DateOccurred 
-        { 
-            get; 
-            protected set;
-        } = DateTimeOffset.UtcNow;
-
-        public bool IsPublished { get; set; }
-
-        #endregion
+        public DateTime DateOccurred { get; protected set; } = DateTime.UtcNow;
     }
 }
